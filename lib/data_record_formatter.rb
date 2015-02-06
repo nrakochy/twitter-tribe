962642ebe69a7d367a65c2ab2b798a767f2c7acf
nrakochy <nick.rakochy@gmail.com>
2015-02-06 14:23:11 -0600

    Added array capacity for data_record formatter and connected to api_interactor

--- lib/data_record_formatter.rb
+++ lib/data_record_formatter.rb
@@ -1,6 +1,12 @@
+require 'json'
 require 'time'
 
 class DataRecordFormatter
+
+  def convert_twitter_results_to_db_format(twitter_ids)
+    time_stamp = Time.now
+    format_twitter_records(twitter_ids, time_stamp)
+  end
 
   def format_twitter_records(twitter_ids, time_stamp)
     twitter_ids.map{ |id| format_individual_id(id, time_stamp) }
--- lib/external_api_interactor.rb
+++ lib/external_api_interactor.rb
@@ -4,6 +4,15 @@ require_relative 'twitter_rest_client'
 class ExternalApiInteractor
 
   def get_all_twitter_followers(username)
+    twitter_ids = execute_twitter_api_call(username)
+    reformat_twitter_results(twitter_ids)
+  end
+
+  def execute_twitter_api_call
     TwitterRestClient.new.get_all_twitter_followers(username).to_json
+  end
+
+  def reformat_twitter_results(twitter_ids)
+    DataRecordFormatter.new.convert_twitter_results_to_db_format(twitter_ids)
   end
 end
