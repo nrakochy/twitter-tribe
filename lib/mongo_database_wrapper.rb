790e780c4c6180cc519030a814423e4f6ea2c267
nrakochy <nick.rakochy@gmail.com>
2015-02-06 21:23:18 -0600

    Updated client variable in MongoDB wrapper

--- lib/mongo_database_wrapper.rb
+++ lib/mongo_database_wrapper.rb
@@ -8,7 +8,7 @@ class MongoDatabaseWrapper
     mongo_uri = ENV['MONGOLAB_URI']
     db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
     @client = MongoClient.from_uri(mongo_uri)
-    @db = client.db(db_name)
+    @db = @client.db(db_name)
     @collection = @db.collection(collection_name)
   end
 
