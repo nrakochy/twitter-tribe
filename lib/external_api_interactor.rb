054c13fcd0743149ce2c6191daf2dda6edceac18
nrakochy <nick.rakochy@gmail.com>
2015-02-09 10:10:48 -0600

    Updated Rest client to call twitter endpoint for unfollower profile

--- lib/external_api_interactor.rb
+++ lib/external_api_interactor.rb
@@ -2,17 +2,36 @@ require_relative 'twitter_rest_client'
 require_relative 'data_record_formatter'
 
 class ExternalApiInteractor
+  
+  def initialize
+    @twitter = TwitterRestClient.new
+    @formatter = DataRecordFormatter.new
+  end
 
   def get_all_twitter_followers(username)
-    twitter_ids = execute_twitter_api_call(username)
-    reformat_twitter_results(twitter_ids)
+    twitter_ids = get_followers_from_twitter(username)
+    find_twitter_ids_from_results(twitter_ids)
   end
 
-  def execute_twitter_api_call(username)
-    TwitterRestClient.new.get_all_twitter_followers(username)
+  def get_followers_from_twitter(username)
+    @twitter.get_all_twitter_followers(username)
   end
 
-  def reformat_twitter_results(twitter_ids)
-    DataRecordFormatter.new.extract_twitter_ids_from_external_results(twitter_ids)
+  def find_twitter_ids_from_results(twitter_ids)
+    @formatter.extract_twitter_ids_from_external_results(twitter_ids)
   end
+
+  def get_twitter_profiles_from_ids(user_ids)
+    twitter_profiles = retrieve_user_information_from_twitter(user_ids)
+    extract_twitter_profile_information(twitter_profiles)
+  end
+
+  def retrieve_user_information_from_twitter(user_ids)
+    @twitter.get_twitter_user_profiles(user_ids)
+  end
+
+  def extract_twitter_profile_information(profiles)
+    @formatter.extract_twitter_users_profiles_info(profiles)
+  end
+
 end
